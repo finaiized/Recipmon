4abfe3b81d24d94cc20c37cd033e3dda1525ce1a
finaiized <finaiized@gmail.com>
2014-06-27 12:09:04 -0700

    Mocking up main screen UI.

--- app/src/main/java/com/finaiized/recipmon/app/MainActivity.java
+++ app/src/main/java/com/finaiized/recipmon/app/MainActivity.java
@@ -9,6 +9,11 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends Activity {
@@ -50,6 +55,8 @@ public class MainActivity extends Activity {
 
     public static class MainActivityFragment extends Fragment {
 
+        private List<String> recipeDataSourceList = new ArrayList<String>();
+
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
@@ -59,6 +66,18 @@ public class MainActivity extends Activity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             return inflater.inflate(R.layout.fragment_main, container, false);
+        }
+
+        @Override
+        public void onActivityCreated(Bundle savedInstanceState) {
+            super.onActivityCreated(savedInstanceState);
+
+            // Mock data
+            recipeDataSourceList.add("Sprinkle Cupcakes");
+
+            ListView listView = (ListView) getActivity().findViewById(R.id.recipeList);
+            listView.setAdapter(new ArrayAdapter<String>(getActivity(),
+                    android.R.layout.simple_list_item_1, recipeDataSourceList));
         }
     }
 }
