a76eb5a573573c89ce82b36c368505942ba597cb
finaiized <finaiized@gmail.com>
2014-06-27 12:09:04 -0700

    Wiring up the 'add recipe' button with appropriate back/up behaviour

--- app/src/main/java/com/finaiized/recipmon/app/AddRecipeActivity.java
+++ app/src/main/java/com/finaiized/recipmon/app/AddRecipeActivity.java
@@ -2,35 +2,26 @@ package com.finaiized.recipmon.app;
 
 import android.app.Activity;
 import android.app.Fragment;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
 
-public class MainActivity extends Activity {
+public class AddRecipeActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_add_recipe);
         if (savedInstanceState == null) {
             getFragmentManager().beginTransaction()
-                    .add(R.id.container_main, new MainActivityFragment())
+                    .add(R.id.container_add_recipe, new AddActivityFragment())
                     .commit();
+            getActionBar().setDisplayHomeAsUpEnabled(true);
+            getActionBar().setTitle("Add Recipe");
         }
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
     }
 
     @Override
@@ -38,27 +29,20 @@ public class MainActivity extends Activity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        switch (item.getItemId()) {
-            case R.id.action_settings:
-                return true;
-            case R.id.add_recipe:
-                startActivity(new Intent(this, AddRecipeActivity.class));
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
         }
         return super.onOptionsItemSelected(item);
     }
 
 
-    public static class MainActivityFragment extends Fragment {
-
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-        }
+    public static class AddActivityFragment extends Fragment {
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            return inflater.inflate(R.layout.fragment_main, container, false);
+            return inflater.inflate(R.layout.fragment_add_recipe, container, false);
         }
     }
 }
